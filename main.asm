8ebafbb630e65221711300f0e4e5add97d811bf0
kameha1011 <omarcitofc2001@gmail.com>
2024-11-07 23:47:01 -0300

    eliminados comentarios viejos

--- main.asm
+++ main.asm
@@ -1,13 +1,5 @@
 global main
-extern printf
-extern puts
 section .data
-    ; Definimos la matriz 7x7
-    ; 0 = casillas nulas (no se pintan en pantalla)
-    ; 1 = casillas vacias
-    ; 'X' = soldados
-    ; 'O' = oficiales
-    ; casillas nulas a11 a12 a21 a22 a16 a17 a26 a27 a61 a62 a71 a72 a66 a67 a76 a77
     global matriz
     matriz db  0,   0,  88, 88, 88,  0,   0  ; Fila 1
            db  0,   0,  88, 88, 88,  0,   0  ; Fila 2
