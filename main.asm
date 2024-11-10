f529738561bc456a4fce26a245e5eafab49c3d0e
AGUST1N18 <mduarte@fi.uba.ar>
2024-11-09 22:47:28 -0300

    validacion de casillas - v1.1

--- main.asm
+++ main.asm
@@ -1,20 +1,25 @@
 global main
 %include "menu.inc"
 ;%include "display.inc"
+
+extern printf
+extern scanf
+extern sscanf
+
 section .data
-    turno db 1  ;0 si es turno del jugador 1, 1 si es turno del jugador 2
-    posicion_soldado dw 0 ;posicion del soldado elegido para el turno
-    posicion_oficial dw 0 ;posicion del oficial elegido para el turno
+    turno db 0  ;0 si es turno del jugador 1, 1 si es turno del jugador 2
+    posicion_soldado dw " ";posicion del soldado elegido para el turno
+    posicion_oficial dw " " ;posicion del oficial elegido para el turno
     formato_casilla db "Elegiste el oficial de la %hi, y quieres moverlo a la casilla %hi",10, 0
 
     global matriz ;0 casilla invalida - 88 casilla soldado - 79 casilla oficial - 1 casilla vacia
-    matriz db  0,   0,  88, 88, 88,  0,   0  ; Fila 1
-           db  0,   0,  88, 88, 88,  0,   0  ; Fila 2
+    matriz db  0,   0,  88, 88, 88,  0,   0  ; Fila 0
+           db  0,   0,  88, 88, 88,  0,   0  ; Fila 1
+           db 88,  88,  88, 88, 88, 88,  88  ; Fila 2
            db 88,  88,  88, 88, 88, 88,  88  ; Fila 3
-           db 88,  88,  88, 88, 88, 88,  88  ; Fila 4
-           db 88,  88,   1,  1,  1, 88,  88  ; Fila 5
-           db  0,   0,   1,  1, 79,  0,   0 ; Fila 6
-           db  0,   0,  79,  1,   1, 0,   0   ; Fila 7ce
+           db 88,  88,   1,  1,  1, 88,  88  ; Fila 4
+           db  0,   0,   1,  1, 79,  0,   0  ; Fila 5
+           db  0,   0,  79,  1,   1, 0,   0  ; Fila 6ce
     filas dd 7
     columnas dd 7
     longitudFila dd 7
@@ -22,7 +27,7 @@ section .data
     oficial db 79
     soldado db 88
 section .bss
-    casilla resw 1 ;casilla a la que se quiere mover el jugador
+    casilla resb 2 ;casilla a la que se quiere mover el jugador
 
 section .text
 main:
@@ -34,11 +39,5 @@ main:
     call menu_juego
     add rsp,8
 
-    mov rdi, formato_casilla
-    mov rsi, [posicion_oficial]
-    mov rdx, [casilla]
-    sub rsp, 8
-    call printf
-    add rsp, 8
 
     ret
