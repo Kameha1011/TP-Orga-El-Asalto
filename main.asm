2381bc226a96be346a22655c79fbeb9d057ff7ce
AGUST1N18 <mduarte@fi.uba.ar>
2024-11-22 15:33:43 -0300

    checks de juego terminado, quien gano. eliminacion de oficial, por no cumplir con eliminar soldado en caso que pueda, base menu principal req 3

--- main.asm
+++ main.asm
@@ -7,7 +7,12 @@ extern scanf
 extern sscanf
 
 section .data
-    turno db 1  ;0 si es turno del jugador 1(soldado), 1 si es turno del jugador 2(oficial)
+    soldados_insuficientes db "No quedan suficientes soldados, los oficiales ganan la partida",10,0
+    oficiales_insuficientes db "No quedan oficiales, los soldados ganan la partida",10,0
+    fuerte_tomado db "El fuerte ha sido tomado, los soldados ganan la partida",10,0
+
+
+    turno db 0  ;0 si es turno del jugador 1(soldado), 1 si es turno del jugador 2(oficial)
     casilla_destino dw "",0 ;casilla a la cual se quiere mover el jugador
     formato_casilla db "Elegiste el oficial de la %hi, y quieres moverlo a la casilla %hi",10, 0
 
@@ -15,8 +20,8 @@ section .data
     matriz db  0,   0,  88, 88, 88,  0,   0  ; Fila 0
            db  0,   0,  88, 88, 88,  0,   0  ; Fila 1
            db 88,  88,  88, 88, 88, 88,  88  ; Fila 2
-           db 88,  88,  88, 88, 88, 88,  88  ; Fila 3
-           db 88,  88,   1,  1,  1, 88,  88  ; Fila 4
+           db 88,  88,  88, 88, 1, 88,  88  ; Fila 3
+           db 88,  88,   1,  1, 88, 88,  88  ; Fila 4
            db  0,   0,   1,  1, 79,  0,   0  ; Fila 5
            db  0,   0,  79,  1,   1, 0,   0  ; Fila 6ce
     filas dd 7
@@ -25,6 +30,12 @@ section .data
     longitudElemento dd 1
     num_oficial db 79
     num_soldado db 88
+
+
+    soldados_en_fuerte db 0
+    cantidad_oficiales db 2
+    cantidad_soldados db 24
+
     vacio db 1
     sys_call dq 60
     exit_code dq 0
@@ -38,16 +49,57 @@ main:
     add rsp,8
 
     sub rsp, 8
-    call menu_juego
+    call empezar_juego
     add rsp,8
 
     cmp rax,[sys_call]
     je fin
+    cmp byte[soldados_en_fuerte],9
+    je fin_fuerte
+    cmp byte[cantidad_soldados],8
+    je fin_soldado
+    cmp byte[cantidad_oficiales],0
+    je fin_oficial
     jmp main
-
 
     ret
 fin:
+    mov rax,[sys_call]
+    mov rdi,[exit_code]
+    syscall
+fin_oficial:
+    sub rsp, 8
+    call imprimir_tablero
+    add rsp,8
+    mov rdi, oficiales_insuficientes
+    sub rsp, 8
+    call printf
+    add rsp, 8
+
+    mov rax,[sys_call]
+    mov rdi,[exit_code]
+    syscall
+fin_soldado:
+    sub rsp, 8
+    call imprimir_tablero
+    add rsp,8
+    mov rdi, soldados_insuficientes
+    sub rsp, 8
+    call printf
+    add rsp, 8
+
+    mov rax,[sys_call]
+    mov rdi,[exit_code]
+    syscall
+fin_fuerte:
+    sub rsp, 8
+    call imprimir_tablero
+    add rsp,8
+    mov rdi, fuerte_tomado
+    sub rsp, 8
+    call printf
+    add rsp, 8
+
     mov rax,[sys_call]
     mov rdi,[exit_code]
     syscall
