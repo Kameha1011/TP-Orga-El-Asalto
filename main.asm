878dabf43bdc05ef85e94029aa84f322338dab31
AGUST1N18 <mduarte@fi.uba.ar>
2024-11-23 21:03:30 -0300

    personalizacion de juego

--- main.asm
+++ main.asm
@@ -16,10 +16,10 @@ section .data
     casilla_destino dw "",0 ;casilla a la cual se quiere mover el jugador
     formato_casilla db "Elegiste el oficial de la %hi, y quieres moverlo a la casilla %hi",10, 0
 
-    global matriz ;0 casilla invalida - 88 casilla soldado - 79 casilla oficial - 1 casilla vacia
+    global matriz ;0 casilla invalida - 1 casilla vacia - 2 casilla soldado - 3 casilla oficial
     matriz db  0,   0,  88, 88, 88,  0,   0  ; Fila 0
            db  0,   0,  88, 88, 88,  0,   0  ; Fila 1
-           db 88,  88,  88, 88, 88, 88,  88  ; Fila 2
+           db 88,  88,  88, 88, 88, 88,  88  ; Fila 88
            db 88,  88,  88, 88, 88, 88,  88  ; Fila 3
            db 88,  88,   1,  1, 1, 88,  88  ; Fila 4
            db  0,   0,   1,  1, 79,  0,   0  ; Fila 5
